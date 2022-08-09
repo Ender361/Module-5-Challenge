843eb0fd8c4ef32866bfe4abd4b4f70c4d053f3e
Ender361 <108046910+Ender361@users.noreply.github.com>
2022-08-08 23:40:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// var currentDay = moment();
+
+// console.log(currentDay);
+
+// $("currentDay").text("poop");
+
+var currentDay = moment().format("MMM Do, YYYY");
+$("#currentDay").text(currentDay);
+
+
